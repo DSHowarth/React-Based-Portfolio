984ebfae57446666902f63aa9e37cb7a2d03f04e
Dash <dkshowarth@gmail.com>
2023-08-30 14:55:28 -0700

    add router functionality, remove extra default vite code

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './css/App.css'
+import Header from './components/Header'
+import {Outlet} from 'react-router-dom'
+import Footer from './components/Header'
 
-function App() {
+export default function App() {
   return (
-
+    <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </>
   )
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './css/index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Portfolio/>
+      }
+      {
+        path: '/About',
+        element: <About/>
+      },
+      {
+        path: '/Contact',
+        element: <Contact/>
+      },
+      {
+        path: '/Resume',
+        element: <Resume/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+
 )
