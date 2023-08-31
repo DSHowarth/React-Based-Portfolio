3fd3ce86c6173d13581483acb1e1027a1e2c2c35
Dash <dkshowarth@gmail.com>
2023-08-31 16:53:08 -0700

    add Resume page skeleton

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,23 @@
 
 export default function Resume() {
     return(
-        <div></div>
+        <div className='container'>
+            <a href="https://www.youtube.com/watch?v=0H25ve3qts4">Download my full Resume here.</a>
+
+            <h3>Front End Proficencies</h3>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Bootstrap</li>
+                <li>React</li>
+            </ul>
+            <h3>Back End Proficencies</h3>
+            <ul>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>MongoDB/Mongoose</li>
+                <li>MySQL/Sequelize</li>
+            </ul>
+        </div>
     )
 }
