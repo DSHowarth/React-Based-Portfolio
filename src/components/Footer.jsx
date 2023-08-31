2604c1912715b5fa36d267bd91dadd08d9cc748d
Dash <dkshowarth@gmail.com>
2023-08-31 16:38:57 -0700

    add links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,12 @@
 
 export default function Footer() {
     return (
-        <div></div>
+        <div className="container">
+            <ul>
+                <li><a href="https://github.com/DSHowarth">Github</a></li>
+                <li><a href="https://www.linkedin.com/in/derek-stanley-howarth-933a46106/">LinkedIn</a></li>
+                <li><a href="https://raider.io/characters/us/magtheridon/Infiniteseer">Raider.io</a></li>
+            </ul>
+        </div>
     )
 }
