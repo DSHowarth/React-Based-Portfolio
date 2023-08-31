31c954d1fd7e412bd08a85378d4c5f122efc096d
Dash <dkshowarth@gmail.com>
2023-08-30 17:52:00 -0700

    add react-router functionality to nav bar, add content to About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,15 @@
+import Container from 'react-bootstrap/Container';
 
 export default function About() {
     return(
-        <div></div>
+        <Container className='bg-dark'>
+            <h1 className='fs-1'>About Me</h1>
+            <p>Another portfolio, and I am STILL not ready to write an About Me page during the assignment itself.
+                I mean, seriously, I&apos;m doing this assignment a bit early because it&apos;s actually due on my
+                gosh darn birthday. Right before a 3 day weekend! Everything about this assignment is filler to meet
+                the requirements so I can go enjoy my last birthday before turning 30. This won&apos;t be here when I&apos;m
+                applying for jobs, honest. 
+            </p>
+        </Container>
     )
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,29 +1,20 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
+import {LinkContainer} from 'react-router-bootstrap';
 
 export default function Navigation() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand>Derek Stanley-Howarth</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <LinkContainer to='/'><Nav.Link>Home</Nav.Link></LinkContainer>
+              <LinkContainer to='/About'><Nav.Link>About Me</Nav.Link></LinkContainer>
+              <LinkContainer to='/Contact'><Nav.Link>Contact</Nav.Link></LinkContainer>
+              <LinkContainer to='/Resume'><Nav.Link>Resume</Nav.Link></LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
