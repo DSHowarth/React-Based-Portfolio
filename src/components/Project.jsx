3d5d0ab45485d15c9d969b8000a085da4110957c
Dash <dkshowarth@gmail.com>
2023-08-31 16:57:16 -0700

    create project skeleton

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,11 @@
 
-export default function Project() {
+export default function Project({name, link, img, alt}) {
     return(
-        <div></div>
+        <div>
+            <a className="project" href={link}>
+                <h4>{name}</h4>
+                <img src={img} className="backgroundimg" alt={alt}/>
+            </a>
+        </div>
     )
 }
