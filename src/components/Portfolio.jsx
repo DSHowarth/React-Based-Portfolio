3dbed111596270666b537cfb12249eb549b42734
Dash <dkshowarth@gmail.com>
2023-09-01 16:58:21 -0700

    add repo links to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,7 +9,7 @@ export default function Portfolio() {
         <div className="container">
             {/* create a project component for every item in my portfolio array */}
             {projects.map((item, index) => {
-                return <Project name={item.name} link={item.link} img={item.img} key={index}/>
+                return <Project name={item.name} link={item.link} img={item.img} gitHub={item.gitHub} key={index}/>
             })}
         </div>
     )
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,11 +1,15 @@
 
-export default function Project({name, link, img, alt}) {
+export default function Project({name, link, img, alt, gitHub}) {
     return(
         // a div with links and images according to the props
         <div>
             <a className="project" href={link}>
                 <h4>{name}</h4>
+
                 <img src={img} className="backgroundimg" alt={alt}/>
+            </a>
+            <a href={gitHub}>
+                <h4>Github Repo</h4>
             </a>
         </div>
     )
