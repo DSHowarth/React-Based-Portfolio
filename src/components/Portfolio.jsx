49f631a30c8f17158fa2cf73fd6489c54cc9885d
Dash <dkshowarth@gmail.com>
2023-08-31 17:08:41 -0700

    add portfolio/project skeletons

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,6 +1,14 @@
+import Project from './Project'
+import projects from './projectData'
 
 export default function Portfolio() {
+
+
     return(
-        <div></div>
+        <div className="container">
+            {projects.map((item, index) => {
+                return <Project name={item.name} link={item.link} img={item.img} key={index}/>
+            })}
+        </div>
     )
 }
---
+++ src/components/projectData.js
@@ -0,0 +1,40 @@
+const projects = [
+    {
+        name: "The Monkey's Cup",
+        link: "https://sheltered-scrubland-47894-17ae2d63c103.herokuapp.com/",
+        img: "./assets/img.png",
+        alt: 'Image of deployed site'
+    },
+    {
+        name: "Non-React Portfolio",
+        link: "https://dshowarth.github.io/Portfolio/",
+        img: "./assets/img.png",
+        alt: 'Image of deployed site'
+    },
+    {
+        name: "Is Ignorance Bliss?",
+        link: "https://popiuy.github.io/server-side-api-project/",
+        img: "./assets/img.png",
+        alt: 'Image of deployed site'
+    },
+    {
+        name: "Placeholder 1",
+        link: "https://www.google.com/",
+        img: "./assets/img.png",
+        alt: 'Image of deployed site'
+    },
+    {
+        name: "Placeholder 2",
+        link: "https://www.google.com/",
+        img: "./assets/img.png",
+        alt: 'Image of deployed site'
+    },
+    {
+        name: "Placeholder 3",
+        link: "https://www.google.com/",
+        img: "./assets/img.png",
+        alt: 'Image of deployed site'
+    },
+]
+
+export default projects
