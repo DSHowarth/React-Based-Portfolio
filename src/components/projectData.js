8fbc89efeec8bab800d9072f3ce439fd7885a03c
Dash <dkshowarth@gmail.com>
2023-08-31 17:12:50 -0700

    add portfolio pictures, fix image links

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -2,37 +2,37 @@ const projects = [
     {
         name: "The Monkey's Cup",
         link: "https://sheltered-scrubland-47894-17ae2d63c103.herokuapp.com/",
-        img: "./assets/img.png",
+        img: "./src/assets/MonkeyCup.png",
         alt: 'Image of deployed site'
     },
     {
         name: "Non-React Portfolio",
         link: "https://dshowarth.github.io/Portfolio/",
-        img: "./assets/img.png",
+        img: "./src/assets/dazzle.png",
         alt: 'Image of deployed site'
     },
     {
         name: "Is Ignorance Bliss?",
         link: "https://popiuy.github.io/server-side-api-project/",
-        img: "./assets/img.png",
+        img: "./src/assets/IiB.png",
         alt: 'Image of deployed site'
     },
     {
         name: "Placeholder 1",
         link: "https://www.google.com/",
-        img: "./assets/img.png",
+        img: "./src/assets/bees.jpg",
         alt: 'Image of deployed site'
     },
     {
         name: "Placeholder 2",
         link: "https://www.google.com/",
-        img: "./assets/img.png",
+        img: "./src/assets/beetle.jpg",
         alt: 'Image of deployed site'
     },
     {
         name: "Placeholder 3",
         link: "https://www.google.com/",
-        img: "./assets/img.png",
+        img: "./src/assets/fire.jpg",
         alt: 'Image of deployed site'
     },
 ]
