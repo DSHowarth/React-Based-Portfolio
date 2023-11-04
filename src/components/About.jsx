49bedab3de979f008cf9f7b048aa9fb66430cc5e
Dash <dkshowarth@gmail.com>
2023-11-04 13:38:41 -0700

    finish hobbies section sans styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,11 +13,20 @@ export default function About() {
                 <li>Earned a certificate in Full Stack Web Development from UC Berkeley Extension. </li>
                 <li>Self-sufficient problem solver with a passion for finding clean and elegant solutions. </li>
                 <li>Excels at oral/written communication, especially acting as a bridge between technical and non-technical parties.</li>
-            </ul> 
-
+            </ul>
             </Row>
-
+            <Row>
+            <p>When I'm not coding, I'm</p>
+            <ul>
+            <li>Playing PC games like World of Warcraft. </li>
+            <ul><li>I'd link my character, but I don't want to dox myself.</li></ul>
+            <li>Reading and writing</li>
+            <li>Battling bad posture from a life at a computer</li>
+            <li>Cooking</li>
+            <ul><li>Here's a pizza I made recently. It has nothing to do with web development, but still!</li></ul>
+            </ul>
             <img src='./assets/Pizza.jpg' className='foodPic' alt='A picture of a very delicious pizza'/>
+            </Row>
         </Container>
     )
 }
