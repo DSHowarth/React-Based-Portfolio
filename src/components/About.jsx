39963040054c568422e4e0b28576649c085d6a42
Dash <dkshowarth@gmail.com>
2023-11-20 15:16:25 -0800

    format picture and bio on About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,27 @@
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row'
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function About() {
     return(
         <Container className='aboutContainer'>
             <h1 className='fs-1'>About Me</h1>
-            <h3>Hello, and welcome to my portfolio.</h3>
             <Row>
-            <img src='./assets/Headshot.jpg' className='bioPic'alt='A very realistic picture of the developer'/>
-            <ul>
-                <li>Full stack web developer with experience working for an established tech company in a development-adjacent role.</li>
-                <li>Earned a certificate in Full Stack Web Development from UC Berkeley Extension. </li>
-                <li>Self-sufficient problem solver with a passion for finding clean and elegant solutions. </li>
-                <li>Excels at oral/written communication, especially acting as a bridge between technical and non-technical parties.</li>
-            </ul>
+                <Col sm md={4} lg={3}>
+                    <img src='./assets/Headshot.jpg' className='bioPic'alt='Picture of the developer'/>
+                </Col>
+                <Col>
+                    <ul>
+                        <li>Full stack web developer with experience working for an established tech company in a development-adjacent role.</li>
+                        <li>Earned a certificate in Full Stack Web Development from UC Berkeley Extension. </li>
+                        <li>Self-sufficient problem solver with a passion for finding clean and elegant solutions. </li>
+                        <li>Excels at oral/written communication, especially acting as a bridge between technical and non-technical parties.</li>
+                    </ul>
+                </Col>
+
             </Row>
             <Row>
-            <h3>When I'm not coding, I'm</h3>
+            <h3>When I'm not coding, I'm:</h3>
             <ul>
             <li>Playing PC games like World of Warcraft.</li>
             <ul><li>I'd link my character, but I don't want to dox myself.</li></ul>
