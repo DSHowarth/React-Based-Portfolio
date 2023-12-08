31cc4d1abdc206b7351093af03faa1569e7858ff
Dash <dkshowarth@gmail.com>
2023-12-08 12:21:02 -0800

    make error page more useful and less snarky

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,8 @@ export default function ErrorMessage() {
     return(
         <>
             <h1>There is Nothing Here</h1>
-            <p> There is nothing here, and I hope that isn't a surprise.</p>
+            <p> There are only a few viable paths for this website. {window.location.pathname} isn't one of them.</p>
+            <a href="https://dstanleyhowarth.netlify.app/">Please click here to return to the home page</a>
         </>
     )
 }
